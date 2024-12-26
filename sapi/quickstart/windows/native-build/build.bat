fe4724f9178de7827dc7df8cc5a0a9d7fe836220
jingjingxyk <zonghengbaihe521@qq.com>
2024-12-27 00:16:05 +0800

    update

--- sapi/quickstart/windows/native-build/build.bat
+++ sapi/quickstart/windows/native-build/build.bat
@@ -24,26 +24,28 @@ rem nmake all
 set X_MAKEFILE=%__PROJECT__%\var\windows-build-deps\php-src\Makefile
 
 
-findstr /C:"x-release-php: " %X_MAKEFILE% >nul
+findstr /C:"x-release-php:" %X_MAKEFILE% >nul
 
 if errorlevel 1 (
-echo custom MAKEFILE x-release-php config!
-goto x-release-php-start
+	echo custom MAKEFILE x-release-php config!
+	goto x-release-php-start
 ) else (
-echo custom MAKEFI file exits !
-goto x-release-php-end
+	echo custom MAKEFI file exits !
+	goto x-release-php-end
 )
 
 :x-release-php-start
 
 
-echo x-build-php-lib^: generated_files  $(PHP_GLOBAL_OBJS) $(CLI_GLOBAL_OBJS) $(STATIC_EXT_OBJS)  $(ASM_OBJS) $(MCFILE) >> %X_MAKEFILE%
-echo x-release-php^: $(DEPS_CLI) $(CLI_GLOBAL_OBJS) x-build-php-lib $(PHP_GLOBAL_OBJS)  $(STATIC_EXT_OBJS) $(ASM_OBJS) $(BUILD_DIR)^\php.exe.res $(BUILD_DIR)^\php.exe.manifest  >> %X_MAKEFILE%
 
+echo x-release-php^:$(DEPS_CLI) $(CLI_GLOBAL_OBJS) $(BUILD_DIR)\$(PHPLIB) $(BUILD_DIR)\php.exe.res $(BUILD_DIR)\php.exe.manifest >> %X_MAKEFILE%
+echo 	@"$(LINK)" /nologo  $(CLI_GLOBAL_OBJS_RESP) $(BUILD_DIR)\$(PHPLIB) $(LIBS_CLI) $(BUILD_DIR)\php.exe.res /out:$(BUILD_DIR)\php.exe $(LDFLAGS) $(LDFLAGS_CLI) >> %X_MAKEFILE%
+echo 	-@$(_VC_MANIFEST_EMBED_EXE) >> %X_MAKEFILE%
 
 
 rem https://www.cnblogs.com/sherry-best/archive/2013/04/15/3022705.html
 rem https://learn.microsoft.com/zh-CN/cpp/c-runtime-library/crt-library-features?view=msvc-170&viewFallbackFrom=vs-2019
+
 rem echo 	^@"$(LINK)" ^/nologo  $(PHP_GLOBAL_OBJS) $(PHP_GLOBAL_OBJS_RESP) $(CLI_GLOBAL_OBJS) $(CLI_GLOBAL_OBJS_RESP)  $(STATIC_EXT_OBJS_RESP)  $(STATIC_EXT_OBJS)  $(ASM_OBJS) $(LIBS) $(LIBS_CLI) $(BUILD_DIR)^\php.exe.res /out:$(BUILD_DIR)^\php.exe $(LDFLAGS) $(LDFLAGS_CLI)    >> %x_makefile%
 rem echo 	^@"$(LINK)" ^/nologo $(DEPS_CLI)  $(STATIC_EXT_OBJS_RESP)  $(STATIC_EXT_LIBS)  $(ASM_OBJS) $(LIBS) $(LIBS_CLI)    $(BUILD_DIR)^\php.exe.res  /out:$(BUILD_DIR)^\php.exe $(LDFLAGS) $(LDFLAGS_CLI)  >> %x_makefile%
 rem echo 	-@$(_VC_MANIFEST_EMBED_EXE)   >> %x_makefile%
@@ -63,7 +65,7 @@ rem /MANIFEST:php.exe.manifest /MANIFESTUAC:uiAccess /SUBSYSTEM:CONSOLE  /subsys
 
 rem nmake /E x-release-php
 :: nmake /E php.exe
-nmake x-build-php-lib
+nmake /E x-release-php
 
 
 :: .\x64\Release\php.exe -v
