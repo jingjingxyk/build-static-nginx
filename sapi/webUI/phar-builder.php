6c8f71c0023f55dcd16fa4c471e5b91d6a5b94d4
jingjingxyk <zonghengbaihe521@qq.com>
2025-01-03 21:16:31 +0800

    update

--- sapi/webUI/phar-builder.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$phar = new Phar("myapp.phar");
-$phar->buildFromDirectory(__DIR__);
-$phar->startBuffering();
-$phar->setStub($phar->createDefaultStub('bootstrap.php'));
-$phar->stopBuffering();
