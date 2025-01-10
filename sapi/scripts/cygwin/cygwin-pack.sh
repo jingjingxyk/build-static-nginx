8d8a2c39035861c54f36b4152076da781b34e1ea
jingjingxyk <zonghengbaihe521@qq.com>
2025-01-10 18:44:23 +0800

    update

--- sapi/scripts/cygwin/cygwin-pack.sh
+++ sapi/scripts/cygwin/cygwin-pack.sh
@@ -15,7 +15,7 @@ cd ${__PROJECT__}/
 ldd ${__PROJECT__}/bin/php.exe
 
 cd ${__PROJECT__}
-APP_VERSION=$(${__PROJECT__}/bin/php.exe | head -n 1 | awk '{ print $2 }')
+APP_VERSION=$(${__PROJECT__}/bin/php.exe -v | head -n 1 | awk '{ print $2 }')
 NAME="php-v${APP_VERSION}-cygwin-x64"
 
 test -d /tmp/${NAME} && rm -rf /tmp/${NAME}
@@ -23,15 +23,20 @@ mkdir -p /tmp/${NAME}
 mkdir -p /tmp/${NAME}/etc/
 
 cd ${__PROJECT__}/
+ldd ${__PROJECT__}/bin/php.exe | grep -v '/cygdrive/' | awk '{print $3}'
 ldd ${__PROJECT__}/bin/php.exe | grep -v '/cygdrive/' | awk '{print $3}' | xargs -I {} cp {} /tmp/${NAME}/
 
-cp -f ${__PROJECT__}/bin/LICENSE /tmp/${NAME}/
+ls -lh  /tmp/${NAME}/
+
+cp -f ${__PROJECT__}/bin/php.exe /tmp/${NAME}/
+# cp -f ${__PROJECT__}/bin/LICENSE /tmp/${NAME}/
 # cp -f ${__PROJECT__}/bin/credits.html /tmp/${NAME}/
 
 cp -rL /etc/pki/ /tmp/${NAME}/etc/
 
 cd /tmp/${NAME}/
 
+test -f ${__PROJECT__}/${NAME}.zip && rm -f ${__PROJECT__}/${NAME}.zip
 zip -r ${__PROJECT__}/${NAME}.zip .
 
 cd ${__PROJECT__}
