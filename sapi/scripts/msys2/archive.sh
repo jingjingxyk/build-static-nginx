e52534075c410610ed5cb1f1f9ac1c6be73760fe
jingjingxyk <zonghengbaihe521@qq.com>
2025-01-10 18:35:38 +0800

    update

--- sapi/scripts/msys2/archive.sh
+++ sapi/scripts/msys2/archive.sh
@@ -23,9 +23,12 @@ mkdir -p /tmp/${NAME}
 mkdir -p /tmp/${NAME}/etc/
 
 cd ${__PROJECT__}/
+ldd ${__PROJECT__}/bin/php.exe | grep -v '/c/Windows/' | awk '{print $3}'
 ldd ${__PROJECT__}/bin/php.exe | grep -v '/c/Windows/' | awk '{print $3}' | xargs -I {} cp {} /tmp/${NAME}/
 
-cp -f ${__PROJECT__}/bin/LICENSE /tmp/${NAME}/
+ls -lh  /tmp/${NAME}/
+
+# cp -f ${__PROJECT__}/bin/LICENSE /tmp/${NAME}/
 # cp -f ${__PROJECT__}/bin/credits.html /tmp/${NAME}/
 
 cp -rL /etc/pki/ /tmp/${NAME}/etc/
