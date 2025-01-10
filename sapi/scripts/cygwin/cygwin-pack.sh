b4d067fb1960ddb134d1127aa35058f1ebd5f3b4
jingjingxyk <zonghengbaihe521@qq.com>
2025-01-10 11:03:02 +0800

    add cygwin-pack.sh

--- sapi/scripts/cygwin/cygwin-pack.sh
+++ sapi/scripts/cygwin/cygwin-pack.sh
@@ -20,12 +20,15 @@ NAME="php-v${APP_VERSION}-cygwin-x64"
 
 test -d /tmp/${NAME} && rm -rf /tmp/${NAME}
 mkdir -p /tmp/${NAME}
+mkdir -p /tmp/${NAME}/etc/
 
 cd ${__PROJECT__}/
 ldd ${__PROJECT__}/bin/php.exe | grep -v '/cygdrive/' | awk '{print $3}' | xargs -I {} cp {} /tmp/${NAME}/
 
 cp -f ${__PROJECT__}/bin/LICENSE /tmp/${NAME}/
 # cp -f ${__PROJECT__}/bin/credits.html /tmp/${NAME}/
+
+cp -rL /etc/pki/ /tmp/${NAME}/etc/
 
 cd /tmp/${NAME}/
 
