bfbbba3de8db7ad9c9a1b499946a0c5d721ad508
jingjingxyk <zonghengbaihe521@qq.com>
2025-01-10 11:27:03 +0800

    update

--- sapi/scripts/msys2/archive.sh
+++ sapi/scripts/msys2/archive.sh
@@ -11,4 +11,27 @@ __PROJECT__=$(
 )
 cd ${__PROJECT__}
 
-bin/php sapi/scripts/cygwin/cygwin-pack.php
+cd ${__PROJECT__}/
+ldd ${__PROJECT__}/bin/php.exe
+
+cd ${__PROJECT__}
+APP_VERSION=$(${__PROJECT__}/bin/php.exe | head -n 1 | awk '{ print $2 }')
+NAME="php-v${APP_VERSION}-cygwin-x64"
+
+test -d /tmp/${NAME} && rm -rf /tmp/${NAME}
+mkdir -p /tmp/${NAME}
+mkdir -p /tmp/${NAME}/etc/
+
+cd ${__PROJECT__}/
+ldd ${__PROJECT__}/bin/php.exe | grep -v '/cygdrive/' | awk '{print $3}' | xargs -I {} cp {} /tmp/${NAME}/
+
+cp -f ${__PROJECT__}/bin/LICENSE /tmp/${NAME}/
+# cp -f ${__PROJECT__}/bin/credits.html /tmp/${NAME}/
+
+cp -rL /etc/pki/ /tmp/${NAME}/etc/
+
+cd /tmp/${NAME}/
+
+zip -r ${__PROJECT__}/${NAME}.zip .
+
+cd ${__PROJECT__}
