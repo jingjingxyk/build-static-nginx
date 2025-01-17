b4a1640dc2b4e8dd6a7dc36a97c1ed0bbeab5b07
jingjingxyk <zonghengbaihe521@qq.com>
2025-01-17 23:01:47 +0800

    update

--- setup-swoole-cli-runtime.bat
+++ setup-swoole-cli-runtime.bat
@@ -1,6 +1,6 @@
 rem @echo off
 
-setlocal enableextensions
+setlocal enableextensions  enabledelayedexpansion
 rem show current file location
 echo %~dp0
 cd /d %~dp0
@@ -45,36 +45,59 @@ if not exist "%APP_RUNTIME%" (
 	copy cacert.pem .\%APP_RUNTIME%"
 )
 
+set "original_string=%__PROJECT__%"
+set "string_to_find=\"
+set "string_to_replace=/"
+set modified_string=!original_string:%string_to_find%=%string_to_replace%!
+
+set "original_string=%modified_string%"
+set "string_to_find=:"
+set "string_to_replace="
+set modified_string=!original_string:%string_to_find%=%string_to_replace%!
+
+set "UNIX_PROJECT=/cygdrive/%modified_string%"
+set "PHP_INI=%UNIX_PROJECT%/var/runtime/php.ini"
+
+
 
 (
-echo curl.cainfo=/cygdrive/d/%__PROJECT__%/var/runtime/%APP_RUNTIME%/cacert.pem
-echo openssl.cafile=/cygdrive/d/%__PROJECT__%/var/runtime/%APP_RUNTIME%/cacert.pem
-echo swoole.use_shortname=off
-echo display_errors = On
-echo error_reporting = E_ALL
 
-echo upload_max_filesize=128M
-echo post_max_size=128M
-echo memory_limit=1G
-echo date.timezone=UTC
+	echo curl.cainfo=%UNIX_PROJECT%/var/runtime/%APP_RUNTIME%/cacert.pem
+	echo openssl.cafile=%UNIX_PROJECT%/var/runtime/%APP_RUNTIME%/cacert.pem
+	echo swoole.use_shortname=off
+	echo display_errors = On
+	echo error_reporting = E_ALL
 
-echo opcache.enable=On
-echo opcache.enable_cli=On
-echo opcache.jit=1225
-echo opcache.jit_buffer_size=128M
+	echo upload_max_filesize=128M
+	echo post_max_size=128M
+	echo memory_limit=1G
+	echo date.timezone=UTC
 
+	echo opcache.enable=On
+	echo opcache.enable_cli=On
+	echo opcache.jit=1225
+	echo opcache.jit_buffer_size=128M
 
-echo expose_php=Off
-echo apc.enable_cli=1
+	echo expose_php=Off
+	echo apc.enable_cli=1
+
 ) > php.ini
 
+echo %PHP_INI%
 %__PROJECT__%\var\runtime\%APP_RUNTIME%\%APP_RUNTIME%\bin\swoole-cli.exe -h
+%__PROJECT__%\var\runtime\%APP_RUNTIME%\%APP_RUNTIME%\bin\swoole-cli.exe -c %PHP_INI% -v
+%__PROJECT__%\var\runtime\%APP_RUNTIME%\%APP_RUNTIME%\bin\swoole-cli.exe -c %PHP_INI% -m
+%__PROJECT__%\var\runtime\%APP_RUNTIME%\%APP_RUNTIME%\bin\swoole-cli.exe -c %PHP_INI% --ri curl
+%__PROJECT__%\var\runtime\%APP_RUNTIME%\%APP_RUNTIME%\bin\swoole-cli.exe -c %PHP_INI% --ri openssl
+%__PROJECT__%\var\runtime\%APP_RUNTIME%\%APP_RUNTIME%\bin\swoole-cli.exe -c %PHP_INI% --ri swoole
+%__PROJECT__%\var\runtime\%APP_RUNTIME%\%APP_RUNTIME%\bin\swoole-cli.exe -c %PHP_INI% %UNIX_PROJECT%/var/runtime/composer.phar list
+
+cd %__PROJECT__%
 
 
-%__PROJECT__%\var\runtime\%APP_RUNTIME%\%APP_RUNTIME%\bin\swoole-cli.exe -c %__PROJECT__%\var\runtime\%APP_RUNTIME%\php.ini -v
-%__PROJECT__%\var\runtime\%APP_RUNTIME%\%APP_RUNTIME%\bin\swoole-cli.exe -c %__PROJECT__%\var\runtime\%APP_RUNTIME%\php.ini -m
-%__PROJECT__%\var\runtime\%APP_RUNTIME%\%APP_RUNTIME%\bin\swoole-cli.exe -c %__PROJECT__%\var\runtime\%APP_RUNTIME%\php.inii --ri swoole
-%__PROJECT__%\var\runtime\%APP_RUNTIME%\%APP_RUNTIME%\bin\swoole-cli.exe -c %__PROJECT__%\var\runtime\%APP_RUNTIME%\php.ini --ri openssl
+%__PROJECT__%\var\runtime\%APP_RUNTIME%\%APP_RUNTIME%\bin\swoole-cli.exe -c %PHP_INI% %UNIX_PROJECT%/var/runtime/composer.phar config -g repos.packagist composer https://mirrors.tencent.com/composer/
+%__PROJECT__%\var\runtime\%APP_RUNTIME%\%APP_RUNTIME%\bin\swoole-cli.exe -c %PHP_INI% %UNIX_PROJECT%/var/runtime/composer.phar install  -vvv --prefer-install=dist --download-only --ignore-platform-req=hhvm
 
+:: %__PROJECT__%\var\runtime\%APP_RUNTIME%\%APP_RUNTIME%\bin\swoole-cli.exe -c %PHP_INI% %UNIX_PROJECT%/var/runtime/composer.phar config -g repos.packagist composer https://packagist.org
 
 endlocal
