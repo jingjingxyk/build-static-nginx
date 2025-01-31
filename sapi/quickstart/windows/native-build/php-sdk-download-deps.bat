32fa0bc3f62b36f01c40fd6f5eb9df9a96087568
jingjingxyk <zonghengbaihe521@qq.com>
2025-01-31 18:57:57 +0800

    test

--- sapi/quickstart/windows/native-build/php-sdk-download-deps.bat
+++ sapi/quickstart/windows/native-build/php-sdk-download-deps.bat
@@ -7,7 +7,13 @@ cd /d ..\..\..\..\
 
 set "__PROJECT__=%cd%"
 echo %cd%
+
+set "PATH=%PATH%;%__PROJECT__%\var\windows-build-deps\php-sdk-binary-tools\bin;"
+set "PATH=%PATH%;%__PROJECT__%\var\windows-build-deps\php-sdk-binary-tools\bin\php;"
+
 cd /d %__PROJECT__%\var\windows-build-deps\php-sdk-binary-tools\
+
+echo "extension_dir=%__PROJECT__%\var\windows-build-deps\php-sdk-binary-tools\bin\php\ext\" >> %__PROJECT__%\var\windows-build-deps\php-sdk-binary-tools\bin\php\php.ini
 
 call phpsdk_buildtree phpdev
 
