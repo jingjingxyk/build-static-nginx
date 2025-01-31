ff6b7d2a2cfb423b3b0535b09a1f87130c8c0ff3
jingjingxyk <zonghengbaihe521@qq.com>
2025-01-31 21:53:26 +0800

    update

--- sapi/quickstart/windows/native-build/x_custom_config.bat
+++ sapi/quickstart/windows/native-build/x_custom_config.bat
@@ -24,7 +24,7 @@ rem -lucrt
 sed.exe -i 's/\/LD \/MD/\/MT/' %X_MAKEFILE%
 sed.exe -i 's/\/D _USRDLL/ /' %X_MAKEFILE%
 sed.exe -i 's/ZEND_DLIMPORT/ /' Zend\zend_stream.c
-sed.exe -i 's/include \"wsyslog\.h\"/include \"syslog\.h\"/' Zend\zend_stream.c
+sed.exe -i 's/ \"wsyslog\.h\"/ \"syslog\.h\"/' Zend\zend_stream.c
 
 findstr /C:"x-show-var: " %X_MAKEFILE%
 findstr /C:"x-show-var: " %X_MAKEFILE% > nul
