5ce772df3cff8874b74c33af5c55c38fad82b518
jingjingxyk <zonghengbaihe521@qq.com>
2025-01-31 11:23:54 +0800

    update

--- sapi/quickstart/windows/native-build/helper/windows-init-install-chromium.bat
+++ sapi/quickstart/windows/native-build/helper/windows-init-install-chromium.bat
@@ -20,9 +20,10 @@ dir
 
 rem https://commondatastorage.googleapis.com/chrome-infra-docs/flat/depot_tools/docs/html/depot_tools_tutorial.html#_setting_up
 :: depot_tools
-git clone -b main --depth=1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
+git clone -b main --depth=1 --progress https://chromium.googlesource.com/chromium/tools/depot_tools.git
 
 SET "PATH=$PATH;%__PROJECT__%\var\windows-build-deps\depot_tools;"
+echo %PATH%
 git config --global user.name "jingjingxyk"
 git config --global user.email "zonghengbaihe521@qq.com"
 git config --global core.autocrlf false
