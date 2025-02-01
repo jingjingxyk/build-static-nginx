756204b0cae5b8f0545b08eaa310eb9448087f1e
jingjingxyk <zonghengbaihe521@qq.com>
2025-02-01 11:41:17 +0800

    update

--- sapi/quickstart/windows/native-build/x_custom_config.bat
+++ sapi/quickstart/windows/native-build/x_custom_config.bat
@@ -24,7 +24,7 @@ rem -lucrt
 sed.exe -i 's/\/LD \/MD/\/MT/' %X_MAKEFILE%
 sed.exe -i 's/\/D _USRDLL/ /' %X_MAKEFILE%
 sed.exe -i 's/ZEND_DLIMPORT/ /' Zend\zend_stream.c
-sed.exe -i 's/: \$\(BUILD_DIR\)\\\$\(PHPDLL\)/: \$\(BUILD_DIR\)\\\$\(PHPDLL\) x-php-lib /' Zend\zend_stream.c
+:: sed.exe -i 's/: \$\(BUILD_DIR\)\\\$\(PHPDLL\)/: \$\(BUILD_DIR\)\\\$\(PHPDLL\) x-php-lib /' Zend\zend_stream.c
 
 findstr /C:"x-show-var: " %X_MAKEFILE%
 findstr /C:"x-show-var: " %X_MAKEFILE% > nul
