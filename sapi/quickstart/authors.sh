fae1a4fc7686a6af9310aa616c94da3a3217ce22
jingjingxyk <zonghengbaihe521@qq.com>
2025-02-02 21:41:00 +0800

    update

--- sapi/quickstart/authors.sh
+++ sapi/quickstart/authors.sh
@@ -1,3 +1,6 @@
 git shortlog -s ${1-} |
 cut -b8- | # strip the commit counts
-sort | uniq
+sort | uniq | cat
+
+# 显示作者和邮箱
+# git shortlog --email --format="%an####<%ae>" | xargs -I {} echo {} | grep "####" | sort | uniq | awk -F "####" '{ print $1 $2 }'
