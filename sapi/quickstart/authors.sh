1f84559ca2c2997409cdc9f27873eb1fc986c25b
jingjingxyk <zonghengbaihe521@qq.com>
2025-02-03 21:06:34 +0800

    add get authors

--- sapi/quickstart/authors.sh
+++ sapi/quickstart/authors.sh
@@ -4,3 +4,6 @@ sort | uniq | cat
 
 # 显示作者和邮箱
 # git shortlog --email --format="%an####<%ae>" | xargs -I {} echo {} | grep "####" | sort | uniq | awk -F "####" '{ print $1 $2 }'
+
+git log --format="%aN <%aE>" | sort -u
+
