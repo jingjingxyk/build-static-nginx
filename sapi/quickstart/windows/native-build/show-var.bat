da971a3583f324b4762ab6ac9718449ea5ca570d
jingjingxyk <zonghengbaihe521@qq.com>
2025-02-04 14:11:32 +0800

    update

--- sapi/quickstart/windows/native-build/show-var.bat
+++ sapi/quickstart/windows/native-build/show-var.bat
@@ -18,10 +18,15 @@ set X_MAKEFILE=%__PROJECT__%\var\windows-build-deps\php-src\Makefile
 dir  %__PROJECT__%\var\windows-build-deps\php-sdk-binary-tools\bin\php
 dir  %__PROJECT__%\var\windows-build-deps\php-sdk-binary-tools\msys2\usr\bin\
 where sed.exe
-:: vswhere.exe -products* -requires
+vswhere.exe -products* -requires
 :: vswhere.exe -legacy -prerelease -format json
 :: vswhere.exe -legacy -prerelease -format json | jq
 :: vswhere.exe -legacy -prerelease
+
+echo %INCLUDE%
+echo %LIB%
+echo %LIBPATH%
+
 
 nmake /f Makefile x-show-var
 
