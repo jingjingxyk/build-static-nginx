576a84fe7ad2b322090e805850d165f09be6e1b8
jingjingxyk <zonghengbaihe521@qq.com>
2025-03-01 21:13:36 +0800

    update openssh build config

--- sapi/src/builder/library/openssh.php
+++ sapi/src/builder/library/openssh.php
@@ -5,18 +5,22 @@ use SwooleCli\Preprocessor;
 
 return function (Preprocessor $p) {
     $openssh_prefix = OPENSSH_PREFIX;
+    $libedit_prefix = LIBEDIT_PREFIX;
+    $zlib_prefix = ZLIB_PREFIX;
+    $openssl_prefix = OPENSSL_PREFIX;
+
     $cflags = $p->getOsType() == 'macos' ? "" : '-static';
     $lib = new Library('openssh');
     $lib->withHomePage('https://www.openssh.com/')
         ->withLicense('https://anongit.mindrot.org/openssh.git/tree/LICENCE', Library::LICENSE_BSD)
         ->withManual('https://www.openssh.com/portable.html')
         ->withDocumentation('https://anongit.mindrot.org/openssh.git')
-        ->withFile('openssh-V_9_9_P1.tar.gz')
+        ->withFile('openssh-V_9_9_P2.tar.gz')
         ->withHttpProxy(true, true)
         ->withDownloadScript(
             'openssh',
             <<<EOF
-                git clone -b V_9_9_P1  --depth=1 git://anongit.mindrot.org/openssh.git
+                git clone -b V_9_9_P2  --depth=1 git://anongit.mindrot.org/openssh.git
 EOF
         )
         ->withPrefix($openssh_prefix)
@@ -26,7 +30,7 @@ EOF
             ./configure --help
             mkdir build
             cd build
-            PACKAGES='zlib openssl '
+            PACKAGES='zlib openssl libedit '
             PACKAGES="\$PACKAGES  "
 
             CPPFLAGS="$(pkg-config  --cflags-only-I  --static \$PACKAGES)" \
@@ -34,6 +38,10 @@ EOF
             LIBS="$(pkg-config      --libs-only-l    --static \$PACKAGES)" \
             ../configure \
             --prefix={$openssh_prefix} \
+            --with-libedit={$libedit_prefix}  \
+            --with-zlib={$zlib_prefix} \
+            --with-ssl-dir={$openssl_prefix} \
+            --enable-year2038 \
             --with-pie
 
             make -j {$p->maxJob}
@@ -42,7 +50,7 @@ EOF
         )
         ->withBuildCached(false)
         ->withInstallCached(false)
-        ->withDependentLibraries('openssl', 'zlib') //'libedit',
+        ->withDependentLibraries('openssl', 'zlib', 'libedit')
         ->disableDefaultLdflags()
         ->disablePkgName()
         ->disableDefaultPkgConfig()
