16972bd44bb8acee303e2fd3a30f0c9c47edf2e6
jingjingxyk <zonghengbaihe521@qq.com>
2025-03-23 10:52:50 +0800

    update

--- sapi/quickstart/macos/homebrew-doctor.sh
+++ sapi/quickstart/macos/homebrew-doctor.sh
@@ -8,6 +8,11 @@ brew missing
 
 exit 0
 
+brew list --formula --built-from-source
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_INSTALL_CLEANUP=1
+
+
 brew untap
 brew untap  Homebrew/homebrew-cask-versions
 brew untap  Homebrew/homebrew-services
