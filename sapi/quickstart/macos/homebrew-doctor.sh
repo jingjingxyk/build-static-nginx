c1abac0487fc618132486b1790276202c84095b3
jingjingxyk <zonghengbaihe521@qq.com>
2025-03-23 10:53:32 +0800

    update

--- sapi/quickstart/macos/homebrew-doctor.sh
+++ sapi/quickstart/macos/homebrew-doctor.sh
@@ -12,6 +12,8 @@ brew list --formula --built-from-source
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 
+# macOS 12 unsupported?
+# https://github.com/orgs/Homebrew/discussions/5603
 
 brew untap
 brew untap  Homebrew/homebrew-cask-versions
