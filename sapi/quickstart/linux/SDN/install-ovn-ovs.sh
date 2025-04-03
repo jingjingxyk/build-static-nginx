5d6ab5ad0e17da607bbbe2c9e3ac6ee4a20ae5cb
jingjingxyk <zonghengbaihe521@qq.com>
2025-04-03 15:39:40 +0800

    update

--- sapi/quickstart/linux/SDN/install-ovn-ovs.sh
+++ sapi/quickstart/linux/SDN/install-ovn-ovs.sh
@@ -176,6 +176,11 @@ fi
 
 cd ${__DIR__}
 
+export LANGUAGE="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+
 cd ${__DIR__}/ovs/
 ./boot.sh
 cd ${__DIR__}/ovs/
