a722fe94e62572653f44f57df021e6bd8e90b097
jingjingxyk <zonghengbaihe521@qq.com>
2025-04-04 20:37:09 +0800

    update

--- setup-openssh-runtime.sh
+++ setup-openssh-runtime.sh
@@ -158,8 +158,8 @@ echo "PidFile ${__PROJECT__}/runtime/openssh/var/run/sshd.pid" >>$SSHD_CONFIG
 
 T__PROJECT__=$(echo "${__PROJECT__}" | sed "s|/|\\\/|g")
 T_APP_RUNTIME_DIR=$(echo "${APP_RUNTIME_DIR}" | sed "s|\/|\\\/|g")
-sed -i '' "s@\/usr\/local\/swoole-cli@${T__PROJECT__}\/runtime@" $SSHD_CONFIG
-sed -i '' "s@\.ssh\/authorized_keys@${T_APP_RUNTIME_DIR}\/\.ssh\/authorized_keys@" $SSHD_CONFIG
+sed -i' ' "s@\/usr\/local\/swoole-cli@${T__PROJECT__}\/runtime@" $SSHD_CONFIG
+sed -i' ' "s@\.ssh\/authorized_keys@${T_APP_RUNTIME_DIR}\/\.ssh\/authorized_keys@" $SSHD_CONFIG
 
 mkdir -p ${APP_RUNTIME_DIR}/var/run/
 mkdir -p ${APP_RUNTIME_DIR}/.ssh/
