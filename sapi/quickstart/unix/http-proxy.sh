d1f99668d5a2d7f36b70ca0f554c98c9d1fecb31
jingjingxyk <zonghengbaihe521@qq.com>
2025-04-05 00:06:02 +0800

    update

--- sapi/quickstart/unix/http-proxy.sh
+++ sapi/quickstart/unix/http-proxy.sh
@@ -9,6 +9,6 @@ cd ${__DIR__}
 
 curl -fSL  https://gitee.com/jingjingxyk/swoole-cli/raw/new_dev/setup-socat-runtime.sh | bash -s -- --mirror china
 
-./bin/runtime/socat -d -d TCP4-LISTEN:8016,reuseaddr,fork ssl:http-proxy.xiaoshuogeng.com:8015,verify=1,\
+./runtime/socat -d -d TCP4-LISTEN:8016,reuseaddr,fork ssl:http-proxy.xiaoshuogeng.com:8015,verify=1,\
 snihost=http-proxy.xiaoshuogeng.com,commonname=http-proxy.xiaoshuogeng.com,\
-openssl-min-proto-version=TLS1.3,openssl-max-proto-version=TLS1.3,cafile=./bin/runtime/cacert.pem
+openssl-min-proto-version=TLS1.3,openssl-max-proto-version=TLS1.3,cafile=./runtime/cacert.pem
