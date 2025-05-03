4ecefe2c899a2176c59a03f283e73a93a10d3a2f
jingjingxyk <zonghengbaihe521@qq.com>
2025-05-04 00:08:26 +0800

    update

--- sapi/quickstart/linux/SDN/OVS/ovn-controller/start.sh
+++ sapi/quickstart/linux/SDN/OVS/ovn-controller/start.sh
@@ -1,46 +1,52 @@
 #!/usr/bin/env bash
 
-__DIR__=$(cd "$(dirname "$0")";pwd)
+__DIR__=$(
+  cd "$(dirname "$0")"
+  pwd
+)
 cd ${__DIR__}
 export PATH=$PATH:/usr/local/share/openvswitch/scripts
 export PATH=$PATH:/usr/local/share/ovn/scripts
 set -exu
 
 PROC_NUM=$(ps -ef | grep 'ovn-controller unix:/usr/local/var/run/openvswitch/db.sock' | grep -v grep | wc -l)
-if test $PROC_NUM -gt 0 ; then
-   echo 'ovn-controller is running ';
-   exit 0
+if test $PROC_NUM -gt 0; then
+  echo 'ovn-controller is running '
+  exit 0
 fi
 
 sh reset.sh
 
-ipv6=$(ip -6 address show  | grep inet6 | awk '{print $2}' | cut -d'/' -f1 | sed -n '2p')
-ipv4=$(ip -4 address show  | grep inet | grep -v 127.0.0 | awk '{print $2}' | cut -d'/' -f1 | sed -n '1p')
+ipv6=$(ip -6 address show | grep inet6 | awk '{print $2}' | cut -d'/' -f1 | sed -n '2p')
+ipv4=$(ip -4 address show | grep inet | grep -v 127.0.0 | awk '{print $2}' | cut -d'/' -f1 | sed -n '1p')
 
 OVN_CENTRAL_IP="192.168.3.251"
 
 HOSTNAME="ovn-node-1"
 EXTERNAL_IP="$ipv4,$ipv6"
 LOCAL_IP="$ipv4,$ipv6"
-ENCAP_TYPE=geneve
+ENCAP_TYPE="geneve"
+# ENCAP_TYPE="geneve,vxlan"
+# mac in udp
+# geneve default port number 6081
+# vxlan  default port number 4789
 
 test -f /usr/local/etc/openvswitch/conf.db && rm -rf /usr/local/etc/openvswitch/conf.db
-test -f /usr/local/etc/ovn/conf.db         && rm -rf /usr/local/etc/ovn/conf.db
+test -f /usr/local/etc/ovn/conf.db && rm -rf /usr/local/etc/ovn/conf.db
 
 ID_FILE=system-id.conf
-test -s $ID_FILE || cat /proc/sys/kernel/random/uuid > $ID_FILE
+test -s $ID_FILE || cat /proc/sys/kernel/random/uuid >$ID_FILE
 
 CHASSIS_NAME=$(cat $ID_FILE)
 ovs-ctl start --system-id=${CHASSIS_NAME}
 
-
 ovs-vsctl set Open_vSwitch . \
-external_ids:system-id="${CHASSIS_NAME}" \
-external_ids:hostname="${HOSTNAME}" \
-external_ids:ovn-encap-ip="${EXTERNAL_IP}" \
-external_ids:ovn-set-local-ip="${LOCAL_IP}" \
-external_ids:ovn-encap-type="${ENCAP_TYPE}" \
-external_ids:ovn-remote="tcp:${OVN_CENTRAL_IP}:6642"
+  external_ids:system-id="${CHASSIS_NAME}" \
+  external_ids:hostname="${HOSTNAME}" \
+  external_ids:ovn-encap-ip="${EXTERNAL_IP}" \
+  external_ids:ovn-set-local-ip="${LOCAL_IP}" \
+  external_ids:ovn-encap-type="${ENCAP_TYPE}" \
+  external_ids:ovn-remote="tcp:${OVN_CENTRAL_IP}:6642"
 
 # external_ids:ovn-nb="tcp:$CENTRAL_IP:6641"
 
@@ -51,9 +57,13 @@ ovn-ctl start_controller
 
 ovs-vsctl --columns external_ids list open_vswitch
 
+ovs-vsctl get open . external-ids
 sleep 5
-ovs-vsctl list-ports  br-int
+ovs-vsctl list-ports br-int
 # ovs-vsctl set int br-int mtu_request=1450
+
+# ovs-vsctl set open . external-ids:system-id={新的chassis ID}
+# ovs-vsctl set open . external-ids:hostname={新的主机名}
 
 ovs-ctl status
 
