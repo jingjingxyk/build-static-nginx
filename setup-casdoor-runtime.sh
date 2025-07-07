b86ddb7d52f5f139abb0905e24e21353ee351e8f
jingjingxyk <zonghengbaihe521@qq.com>
2025-07-07 14:28:38 +0800

    add casdoor runtime

---
+++ setup-casdoor-runtime.sh
@@ -0,0 +1,134 @@
+#!/usr/bin/env bash
+
+set -exu
+__DIR__=$(
+  cd "$(dirname "$0")"
+  pwd
+)
+__PROJECT__=${__DIR__}
+shopt -s expand_aliases
+cd ${__PROJECT__}
+
+OS=$(uname -s)
+ARCH=$(uname -m)
+
+case $OS in
+'Linux')
+  OS="linux"
+  ;;
+'Darwin')
+  OS="darwin"
+  ;;
+*)
+  case $OS in
+  'MSYS_NT'* | 'CYGWIN_NT'*)
+    OS="windows"
+    ;;
+  'MINGW64_NT'*)
+    OS="windows"
+    ;;
+  *)
+    echo '暂未配置的 OS '
+    exit 0
+    ;;
+  esac
+  ;;
+esac
+
+case $ARCH in
+'x86_64')
+  ARCH="amd64"
+  ;;
+'aarch64' | 'arm64')
+  ARCH="arm64"
+  ;;
+*)
+  echo '暂未配置的 ARCH '
+  exit 0
+  ;;
+esac
+
+MIRROR=''
+while [ $# -gt 0 ]; do
+  case "$1" in
+  --proxy)
+    export HTTP_PROXY="$2"
+    export HTTPS_PROXY="$2"
+    NO_PROXY="127.0.0.0/8,10.0.0.0/8,100.64.0.0/10,172.16.0.0/12,192.168.0.0/16"
+    NO_PROXY="${NO_PROXY},::1/128,fe80::/10,fd00::/8,ff00::/8"
+    NO_PROXY="${NO_PROXY},localhost"
+    NO_PROXY="${NO_PROXY},.aliyuncs.com,.aliyun.com,.tencent.com"
+    NO_PROXY="${NO_PROXY},.myqcloud.com,.swoole.com"
+    export NO_PROXY="${NO_PROXY},.tsinghua.edu.cn,.ustc.edu.cn,.npmmirror.com"
+    ;;
+  --*)
+    echo "Illegal option $1"
+    ;;
+  esac
+  shift $(($# > 0 ? 1 : 0))
+done
+
+APP_VERSION='v1.959.0'
+APP_NAME='casdoor'
+VERSION='v1.959.0'
+
+cd ${__PROJECT__}
+mkdir -p runtime/
+mkdir -p var/runtime
+APP_RUNTIME_DIR=${__PROJECT__}/runtime/${APP_NAME}
+mkdir -p ${APP_RUNTIME_DIR}
+
+cd ${__PROJECT__}
+if [[ ! -f ${__PROJECT__}/runtime/node/bin/node ]]; then
+  if [[ "$MIRROR" == "china" ]]; then
+    bash setup-nodejs-runtime.sh --mirror china
+  else
+    bash setup-nodejs-runtime.sh
+  fi
+fi
+if [[ ! -f ${__PROJECT__}/runtime/go/bin/go ]]; then
+  if [[ "$MIRROR" == "china" ]]; then
+    bash setup-go-runtime.sh --mirror china
+  else
+    bash setup-go-runtime.sh
+  fi
+fi
+
+export PATH="${__PROJECT__}/runtime/go/bin/:${__PROJECT__}/runtime/node/bin/:$PATH"
+export GO111MODULE=on
+export GOPATH=~/go
+export GOSUMDB=sum.golang.org
+export GOROOT=${__PROJECT__}/runtime/go/
+export GOBIN=${__PROJECT__}/runtime/go/bin/
+unset GOPROXY
+unset GOSUMDB
+
+go version
+echo $GOPATH
+echo $GOBIN
+
+mkdir -p ${__PROJECT__}/var/runtime/${APP_NAME}/
+cd ${__PROJECT__}/var/runtime/${APP_NAME}/
+APP_RUNTIME="${APP_NAME}-${APP_VERSION}-${OS}-${ARCH}"
+
+if [ -d casdoor ]; then
+  cd casdoor
+  git pull
+else
+  git clone https://github.com/casdoor/casdoor.git
+fi
+
+cd ${__PROJECT__}/var/runtime/${APP_NAME}/casdoor/web/
+ls -lh ~/.npm/
+ls -lh ~/.npm/_logs/
+pwd
+
+export ELECTRON_MIRROR="https://npmmirror.com/mirrors/electron/"
+npm install -g yarn --registry=https://registry.npmmirror.com
+
+yarn install --yes --registry https://registry.npmmirror.com
+yarn build
+
+cd ${__PROJECT__}/var/runtime/${APP_NAME}/casdoor/
+
+go build
